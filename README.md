25e7542663e2638b40a89df56513f377a5d7c97e
Aaron <mail@aosterwyk.com>
2022-12-23 13:23:50 -0600

    added restore mode and changed name from configs to binds

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#  elite-config-backup-script
+#  Elite Binds Backup Script
 
-[![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/aosterwyk/elite-config-backup-script?sort=semver)](https://github.com/aosterwyk/elite-config-backup-script/releases) [![GitHub last commit](https://img.shields.io/github/last-commit/aosterwyk/elite-config-backup-script)](https://github.com/aosterwyk/elite-config-backup-script/commits/master) [![GitHub last commit (branch)](https://img.shields.io/github/last-commit/aosterwyk/elite-config-backup-script/dev?label=last%20commit%20%28dev%29)](https://github.com/aosterwyk/elite-config-backup-script/commits/dev) [![Discord](https://img.shields.io/discord/90687557523771392?color=000000&label=%20&logo=discord)](https://discord.gg/QNppY7T) [![Twitch Status](https://img.shields.io/twitch/status/varixx?label=%20&logo=twitch)](https://twitch.tv/VariXx) 
+[![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/aosterwyk/elite-binds-backup-script?sort=semver)](https://github.com/aosterwyk/elite-binds-backup-script/releases) [![GitHub last commit](https://img.shields.io/github/last-commit/aosterwyk/elite-binds-backup-script)](https://github.com/aosterwyk/elite-binds-backup-script/commits/master) [![GitHub last commit (branch)](https://img.shields.io/github/last-commit/aosterwyk/elite-binds-backup-script/dev?label=last%20commit%20%28dev%29)](https://github.com/aosterwyk/elite-binds-backup-script/commits/dev) [![Discord](https://img.shields.io/discord/90687557523771392?color=000000&label=%20&logo=discord)](https://discord.gg/QNppY7T) [![Twitch Status](https://img.shields.io/twitch/status/varixx?label=%20&logo=twitch)](https://twitch.tv/VariXx) 
 
 A powershell script to backup your Elite Dangerous binds. 
 
@@ -10,16 +10,21 @@ Download or clone the repo and run the script in a powershell window.
 
 If you are seeing execution policy errors use the command below to bypass execution policy:
 
-`powershell.exe -ExecutionPolicy Bypass -File eliteConfigBackup.ps1`
+`powershell.exe -ExecutionPolicy Bypass -File eliteBindsBackup.ps1`
 
 ## Usage
 
 Run the script to copy any .binds files in `%localappdata%\Frontier Developments\Elite Dangerous\Options\Bindings` to the current directory.
 
 ### Switches
-**-force**: The script will add a timestamp to the filename if the file already exists in the backup destination. Use `-force` to overwrite the file instead.
 
-**-backupPath**: The backup will be placed in the working directory (where the script is being run) by default. Use `-backupDestination <location>` to use a different location. 
+**-backup**: Backup mode
+
+**-restore**: Restore mode
+
+**-backupPath**: The backup will be placed in the working directory (where the script is being run) by default. Use `-backupPath <location>` to use a different location. 
+
+**-force**: The script will add a timestamp to the filename if the file already exists in the backup destination. Use `-force` to overwrite the file instead.
 
 ## Support
 
--- eliteConfigBackup.ps1
+++ eliteConfigBackup.ps1
@@ -1,17 +1,17 @@
 param(
     [switch]$force,
+    [switch]$backup,
     [string]$backupPath,
-    [switch]$resotre,
-    [string]$restorePath,
+    [switch]$restore,
     [switch]$update
 )
 
-$downloadURL = "https://github.com/aosterwyk/elite-config-backup-script/releases/latest"
+$downloadURL = "https://github.com/aosterwyk/elite-binds-backup-script/releases/latest"
 $bindsLocation = "$($Env:localappdata)\Frontier Developments\Elite Dangerous\Options\Bindings"
 
-write-host "Elite Config Backup Tool v0.1.0"
-write-host -foregroundcolor Yellow "This tool does not automatically update. Please periodically check for updates at the link below or use -update to open the downloads page."
-write-host $downloadURL
+write-host -foregroundcolor Cyan "Elite Binds Backup Script v0.1.0"
+write-host -foregroundcolor Cyan $downloadURL
+write-host "This script does not automatically update. Please use -update to open the downloads page to check for updates."
 write-host "`r`no7 CMDR"
 
 if($update) {
@@ -19,35 +19,98 @@ if($update) {
     return
 }
 
-if($backupPath) {
-    write-host "Backup destination set to $($backupPath)."
-}
-else {
-    $backupPath = get-location 
-    write-host "No backup destination set. Using working directory.($($backupPath))"
-    write-host -foregroundcolor cyan "Hint: Use -backupPath to set backup destination"
-}
-
-write-host "`nChecking for binds in $($bindsLocation)`n"
-
-get-childitem "$($bindsLocation)\*.binds" | foreach-object {
-    write-host "Found $($_.name)"
-    if(Test-Path "$($backupPath)\$($_.name)") {
-        write-host -forgroundcolor yellow "File already exists in backup destination."
-        if($force) {
-            write-host -foregroundcolor yellow "Force set. Overwriting file in backup destination."
-            $backupFilename = $_.name
-        }
-        else {
-            $backupFilename = "$($_.basename)-$(get-date -format "yyyyMMdd-HHmmss")$($_.extension)"
-            write-host -foregroundcolor yellow "Using filename $($backupFilename)."
-            write-host -foregroundcolor cyan "Hint: Use -force to ignore this check and overwrite the file."
-        }
+if($restore) {
+    write-host -foregroundcolor green "Restore mode" 
+    
+    # Get location of binds files to be restored 
+    if($backupPath) { 
+        write-host "Restoring binds from $($backupPath)"
     }
     else {
-        # write-host "File $($_.name) does not already exist in backup destination."
-        $backupFilename = $_.name
+        $backupPath = get-location
+        write-host "No backup path set. Restoring binds from working directory ($($backupPath))"
+        write-host -foregroundcolor cyan "Hint: Use -backupPath to set backup location" 
     }
-    copy-item $_ -Destination "$($backupPath)\$($backupFilename)"
-    write-host -foregroundcolor green "Copied to $($backupPath)\$($backupFilename)"
+    
+    # Check for Bindings directory. This may not exist on new installs.
+    if(test-path $bindsLocation) {
+        write-host -foregroundcolor green "Bindings location exists ($($bindsLocation))"
+    }
+    else {
+        write-host -foregroundcolor red "Bindings location does not exit."
+        new-item -path "$($Env:localappdata)\Frontier Developments\Elite Dangerous\Options" -name "Bindings" -itemtype "directory" | out-null
+        write-host -foregroundcolor green "Created directory $($bindsLocation)."
+    }
+    get-childitem "$($backupPath)\*.binds" | foreach-object {
+        write-host "Found $($_.name) in backups directory"
+        if(test-path "$($bindsLocation)\$($_.name)") {
+            write-host -foregroundcolor Yellow "$($_.name) already exists in Bindings directory." 
+            $overwritePrompt = read-host "Overwrite? (y/n)"
+            if($overwritePrompt -eq "y") {
+                write-host -foregroundcolor Yellow "Overwriting $($_.name)" 
+                # write-host -foregroundcolor Cyan "Hint: Use -force to ignore this check and overwrite the file" 
+                # TODO - add -force for restores 
+                copy-item $_ -destination "$($bindsLocation)\$($_.name)" 
+            }
+            else { 
+                write-host "Skipping $($_.name)"
+            }
+        }
+        else {
+            copy-item $_ -destination "$($bindsLocation)\$($_.name)" 
+            write-host "Copied $($_.name) to Bindings directory" 
+        }   
+    }
+    return
 }
+
+if($backup) {
+    # Set location to copy binds files 
+    if($backupPath) {
+        write-host "Backup destination set to $($backupPath)."
+    }
+    else {
+        $backupPath = get-location 
+        write-host "No backup destination set. Using working directory.($($backupPath))"
+        write-host -foregroundcolor cyan "Hint: Use -backupPath to set backup destination"
+    }
+
+    write-host "`nChecking for binds in $($bindsLocation)`n"
+
+    # Get binds files in bindings directory and copy to backup directory
+    get-childitem "$($bindsLocation)\*.binds" | foreach-object {
+        write-host "Found $($_.name)"
+        # TODO - check if backup location exists
+        if(Test-Path "$($backupPath)\$($_.name)") {
+            write-host -forgroundcolor yellow "File already exists in backup destination."
+            if($force) {
+                write-host -foregroundcolor yellow "Force set. Overwriting file in backup destination."
+                $backupFilename = $_.name
+            }
+            else {
+                $backupFilename = "$($_.basename)-$(get-date -format "yyyyMMdd-HHmmss")$($_.extension)"
+                write-host -foregroundcolor yellow "Using filename $($backupFilename)."
+                write-host -foregroundcolor cyan "Hint: Use -force to ignore this check and overwrite the file."
+            }
+        }
+        else {
+            # write-host "File $($_.name) does not already exist in backup destination."
+            $backupFilename = $_.name
+        }
+        copy-item $_ -Destination "$($backupPath)\$($backupFilename)"
+        write-host -foregroundcolor green "Copied to $($backupPath)\$($backupFilename)"
+    }
+    return
+}
+
+write-host "`nHelp
+-backup: Backup mode
+-restore: Restore mode
+-backupPath: (optional) Location to backup or restore backups. Script will use working directory ($(get-location)) if not used. 
+-force: (optional) Force overwriting files if they already exist in the backup or restore directory.
+`nExamples
+Backup binds to c:\temp\elite
+eliteBindsBackup.ps1 -backup -backupPath c:\temp\elite 
+
+Restore binds from c:\temp\elite
+eliteBindsBackup.ps1 -restore -backupPath c:\temp\elite`n"
